38ff9717a9ed039dae73263f762c113c67c85648
Michelle Harley <chell.hx@gmail.com>
2023-06-28 23:47:58 -0400

    its lit, passing all tests

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -17,12 +17,32 @@ router.get("/:index", (req, res) => {
   }
 });
 
-
-
 // POST - CREATE
 router.post("/", (req, res) => {
   logsArray.push(req.body);
-  res.json(logsArray.length - 1);
+  res.status(201).json(logsArray.length - 1);
+});
+
+// DELETE
+router.delete("/:index", (req, res) => {
+  const { index } = req.params;
+  if (!logsArray[index]) {
+    res.status(404).redirect("/logs");
+  } else {
+    let deletedLog = logsArray.splice(index, 1);
+    res.status(200).json(deletedLog);
+  }
+});
+
+// PUT - UPDATE
+router.put("/:index", (req, res) => {
+  const { index } = req.params;
+  if (!logsArray[index]) {
+    res.status(404).redirect("/logs");
+  } else {
+    logsArray[index] = req.body;
+    res.status(200).json(logsArray[index]);
+  }
 });
 
 module.exports = router;
