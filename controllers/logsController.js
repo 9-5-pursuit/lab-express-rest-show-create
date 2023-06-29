e00be3a16fa4b04ac877403786a76ac1036f16f0
davep80 <davidpaq1@gmail.com>
2023-06-29 10:37:04 -0400

    debug paths

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const logsArray = require("../models/log");
+const logsArray = require("../models/logModel");
 
 router.get("/", (req, res) => {
   res.send(logsArray);
+});
+router.get("/:index", (req, res) => {
+  const { index } = req.params;
+
+  if (logsArray[index]) {
+    res.json(logsArray[index])
+  } else {
+    res.redirect('/*')
+  }
 });
 
 module.exports = router;
