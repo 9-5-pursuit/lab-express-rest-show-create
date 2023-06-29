3b2177d241f6b02bdca6e001fd5f66394aeaffdd
davep80 <davidpaq1@gmail.com>
2023-06-29 15:59:52 -0400

    pass six tests

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
-
 const logsController = require("./controllers/logsController");
 
 const app = express();
 
-app.use("/logs", logsController);
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("welcome to the captain's log");
+  res.send("Welcome to the Captian's Log");
 });
 
-app.use("*", (req, res) => {
-  res.status(404).send("Sorry, no page found!");
+app.use("/logs", logsController);
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not Found");
 });
 
 module.exports = app;
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,18 +1,52 @@
 const express = require("express");
 const router = express.Router();
 
-const logsArray = require("../models/logModel");
+let logsArray = require("../models/logs");
 
 router.get("/", (req, res) => {
   res.send(logsArray);
 });
-router.get("/:index", (req, res) => {
-  const { index } = req.params;
 
-  if (logsArray[index]) {
-    res.json(logsArray[index])
+router.get("/:id", (req, res) => {
+  const index = +req.params.id;
+
+  if (!(index in logsArray)) {
+    res.redirect("/logs");
   } else {
-    res.redirect('/*')
+    res.json(logsArray[index]);
+  }
+});
+
+router.post("/", (req, res) => {
+  const newLog = req.body;
+
+  if (!newLog) {
+    res.redirect("/logs");
+  } else {
+    logsArray.push(newLog);
+    res.json(newLog);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const index = +req.params.id;
+
+  if (!(index in logsArray)) {
+    res.redirect("/logs");
+  } else {
+    logsArray.splice(index, 1);
+    res.json(logsArray);
+  }
+});
+
+router.put("/:id", (req, res) => {
+  const index = +req.params.id;
+
+  if (!(index in logsArray)) {
+    res.redirect("/");
+  } else {
+    logsArray.splice(index, 1, req.body);
+    res.json(logsArray);
   }
 });
 
