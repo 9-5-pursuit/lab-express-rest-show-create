3041a78503fdd1f3b1ae9578337971ab794f887d
Jennifer <jenniferpeterson@pursuit.org>
2023-07-04 15:49:36 -0400

    Get an individual view (show one log by index)

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,8 +6,19 @@ const logsArray = require("../models/log");
 
 //Get a list (or index) of all logs
 router.get("/", (req, res) => {
-  console.log(logsArray);
+  // console.log(logsArray);
   res.send(logsArray);
+});
+
+//Get an individual view (show one log by index)
+router.get("/:index", (req, res) => {
+  const { index } = req.params;
+  // console.log(index);
+  if (!logsArray[index]) {
+    res.redirect("/*");
+  } else {
+    res.json(logsArray[index]);
+  }
 });
 
 module.exports = router;
